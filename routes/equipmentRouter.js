939256f15501413946f8459e8e3edacf2a53d3bf
USER-SROD <USER-SROD@Srod-hy-tawat.egat.local>
2020-07-13 23:34:44 +0700

    commit

--- routes/equipmentRouter.js
+++ routes/equipmentRouter.js
@@ -4,6 +4,7 @@ const storage = require('node-sessionstorage');
 var _ = require('lodash');
 var CBModel = require('../models/cbModel');
 var Subs = require('../models/substationModel');
+var LineMessage = require('../models/lineMessageModel');
 
 /* GET equipment first page. */
 router.get('/', function(req, res, next) {
@@ -59,18 +60,48 @@ router.post('/addCB', function(req, res, next) {
     var doc = new CBModel(req.body);
 
     doc.save((err,data)=>{
-    if(err) console.log(err)
-    //console.log(data)
-    res.redirect("/equipment")
+        if(err) console.log(err)
+
+        //add to Linemessage
+        var lineMessageDoc = new LineMessage(
+            {
+                id : data.functional_location,
+                type : "text",
+                name : "info("+data.functional_location+")",
+                message : "FL : "+data.functional_location+"\n"+"Manufacturer : "+data.manufacturer+"\n"+"Model No. : "+data.model_number+"\n"+"Mechanism : "+data.mechanical_model+"\n"+"วันที่เริ่มใช้งาน : "+data.start_date+"\n"+"Description : "+data.description2,
+                data : "",
+                createTime : Date.now(),
+                updateTime : Date.now(),
+                packageId : "",
+                stickerId : "",
+                originalContentUrl : "",
+                previewImageUrl : "",
+                duration : "0",
+                title : "",
+                address : "",
+                latitude : "0",
+                longitude : "0",
+                continue : "",
+                status : "Active"
+            });
+        lineMessageDoc.save((err,dataLine)=>{
+            if(err) console.log(err)
+            //console.log(dataLine)
+            res.redirect("/equipment")
+        })
     })
-    
     
 })
 // post delete
 router.post('/deleteCB/:_id', function(req, res, next){
     CBModel.findByIdAndRemove(req.params._id, (err,data)=>{
       if(err) console.log(err)
-      res.redirect('/equipment')
+      console.log(data)
+      LineMessage.findOneAndDelete({"id":data.functional_location},(err,delete_data)=>{
+          if(err) console.log(err)
+          console.log(delete_data)
+            res.redirect('/equipment')
+        })
   
     })
   })
@@ -95,10 +126,34 @@ router.get('/editCB/:_id', function(req, res, next){
 //update post
 router.post('/editCB/:_id', function(req, res, next){
     //console.log(req.body)
-    CBModel.findByIdAndUpdate(req.params._id, req.body, (err,data)=>{
-      if(err) console.log(err)
-     
-      res.redirect("/equipment")
+    CBModel.findByIdAndUpdate(req.params._id, req.body, (err,old_data)=>{
+        if(err) console.log(err)
+        CBModel.findOne({"_id":req.params._id},(err,data)=>{
+            if(err) console.log(err)
+            console.log(data)
+            LineMessage.findOneAndUpdate({"id":old_data.functional_location},{
+                id : data.functional_location,
+                type : "text",
+                name : "info("+data.functional_location+")",
+                message : "FL : "+data.functional_location+"\n"+"Manufacturer : "+data.manufacturer+"\n"+"Model No. : "+data.model_number+"\n"+"Mechanism : "+data.mechanical_model+"\n"+"วันที่เริ่มใช้งาน : "+data.start_date+"\n"+"Description : "+data.description2,
+                data : "",
+                createTime : Date.now(),
+                updateTime : Date.now(),
+                packageId : "",
+                stickerId : "",
+                originalContentUrl : "",
+                previewImageUrl : "",
+                duration : "0",
+                title : "",
+                address : "",
+                latitude : "0",
+                longitude : "0",
+                continue : "",
+                status : "Active"
+            },(err,update_data)=>{
+                res.redirect("/equipment")
+            })
+        })
     })
   })
 
